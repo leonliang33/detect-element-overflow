36585bc324c2a05e1e05ad7d64773817d078736c
Leon Liang <leli@microsoft.com>
2020-03-24 16:36:38 -0700

    Add types for typescript

---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+declare namespace detectElementOverflow {
+    collidedTop: boolean;
+    collidedBottom: boolean;
+    collidedLeft: boolean;
+    collidedRight: boolean;
+    overflowTop: number;
+    overflowBottom: number;
+    overflowLeft: number;
+    overflowRight: number
+}
